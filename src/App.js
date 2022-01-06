8889de6923ca576a6ccacca489ff7125f2244be3
abhisheksk1 <73150402+abhisheksk1@users.noreply.github.com>
2022-01-06 15:05:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import {BrowserRouter as Router} from "react-router-dom";
+import Login from './components/login/Login';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Sidebar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Login from './components/login/Login';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
