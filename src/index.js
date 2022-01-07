e9a63f52dee2c35c93129070750e9e0d86549f41
‘Raji <base36.rr@gmail.com>
2022-01-07 11:54:10 +0100

    removed login/login

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Login from './components/login/Login';
 
 ReactDOM.render(
   <React.StrictMode>
