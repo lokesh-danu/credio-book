55cda375bf60c7615858b182803d78b254502572
‘Raji <base36.rr@gmail.com>
2022-01-07 12:06:16 +0100

    resolved

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { createContext, useState } from "react";
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
+import {
+  BrowserRouter as Router, Route, Routes,
+  useRoutes,
+} from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
+import Deposit from "./components/create-deposit/Deposit";
+import CreateUser from "./components/create-user/Create";
+import Transaction from "./components/transaction/Transaction";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Users from "./components/users/users";
 // import { BrowserRouter as Router } from "react-router-dom";
-
+import Login from './components/Login/Login';
 
 
 function App() {
+
+
   return (
     <Router>
       <Routes>
 
-        {/* <Route exact path="/" element={<Login />} >
+        <Route exact path="/" element={<Login />} >
 
-        </Route> */}
-        <Route exact path="/" element={<Transaction />}>
+        </Route>
+        <Route exact path="/transactions" element={<Transaction />}>
 
         </Route>
         <Route path="/dashboard" element={<Dashboard />}>
@@ -38,6 +47,7 @@ function App() {
       </Routes >
     </Router >
   );
+
 }
 
 export default App;
