37e937621bf16f4aa56583b56270c870e01f9c9b
‘Raji <base36.rr@gmail.com>
2022-01-09 14:18:27 +0100

    added graphy

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -6,6 +6,53 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 import SideBar from "../../components/sidebar/Sidebar";
 import NavBar from "../Navbar/Navbar";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const dataSet = {
+  labels,
+  datasets: [
+    {
+      label: 'Cash flow',
+      data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+
+  ],
+};
 
 const viewportContext = React.createContext({});
 
@@ -136,12 +183,7 @@ class DesktopComponent extends React.Component {
                   <div className='card transaction-card'>
                     <div className='card-body'>
                       <h5 className='card-title'>Cash Flow</h5>
-                      <ul>
-                        {
-
-                        }
-                        <a href="#" className='more'>More...</a>
-                      </ul>
+                      <Bar options={options} data={dataSet} />
                     </div>
                   </div>
                 </div>
