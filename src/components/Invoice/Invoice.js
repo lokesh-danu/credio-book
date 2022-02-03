cc41a75dbf60e256dbebf6e6bdb51368af6165d1
lokesh danu <lokeshdanu@gmail.com>
2022-02-03 21:30:38 +0530

    Invoice mobile page

--- src/components/Invoice/Invoice.js
+++ src/components/Invoice/Invoice.js
@@ -6,6 +6,7 @@ import SideBar from "../sidebar/Sidebar";
 import NavBar from "../Navbar/Navbar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from 'react'
 
 const viewportContext = React.createContext({});
 
@@ -36,48 +37,99 @@ const useViewport = () => {
 };
 
 const MobileComponent = () => {
+    const [data, setData] = useState(null);
+    const [isloaded, setIsloaded] = useState(false);
+    const [overview, setOverview] = useState(true);
+    let usertoken = JSON.parse(sessionStorage.getItem("tokenManager"))
+    // console.log(`data ----- ${data}`)
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `Bearer ${usertoken.token.token}`,
+
+        },
+    };
+    useEffect(() => {
+        fetch(`https://credio-merchant.herokuapp.com/api/v1/credio_store/get/transactions`,
+            requestOptions)
+            .then(results => results.json())
+            .then((transfer) => {
+                // let over = overview.json();
+                console.log(transfer);
+                setData(transfer);
+                setIsloaded(true);
+            }).catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
-        <div>
-            <div className='mobile-display'>
-                <div className='mobile-upper-card'>
-                    <div className='card-body' style={{ display: 'flex', flexDirection: "row" }}>
-                        <h4 className='card-titles'>Recent</h4>
-                        <i class="fas fa-angle-down"></i>
-                    </div>
+        // <></>
+        <>
+            <NavBar navIndex={"Transactions Overview"}></NavBar>
+            <SideBar index='transactions' ></SideBar>
+            {
+                !isloaded && <div className="loading-mobile">
+                    <FontAwesomeIcon
+                        className="spinner mt-6 mb-4"
+                        size="4x"
+                        icon={faSpinner}
+                    ></FontAwesomeIcon>
                 </div>
-                <div className='card deposits-card my-2' style={{ marginLeft: "4rem" }}>
-                    <div className='card-body'>
-                        <h4 className='deposits-card-heading'>Transaction Details</h4>
-                        <div className='line'></div>
-                        <div className='stores-info'>
-                            <img src={Store} alt="" className='store-img' />
-                            <div className='stores-infos'>
-                                <span className='store-nam'>Tony T Stores</span>
-                                <span className='store-mob'>#0000000000</span>
-                            </div>
-                        </div>
-                        <div className='line2'></div>
-                        <div className='cash-detail'>
-                            <span className='deposit-detail'>Deposit</span>
-                            <span className='cash-amt'>$4854.00</span>
-                            <span className='cash-transaction'>Cash Transaction</span>
-                        </div>
-                        <div className='line3'></div>
-                        <div className='description-section'>
-                            <span className='desc-section'>Description</span>
-                        </div>
-                        <div className='line4'></div>
-                        <div className='share-details'>
-                            <button className='btn share-btn'>Share</button>
-                            <div className='shares-details'>
-                                <span className='share-date'>Dec 07, 2021</span>
-                                <span className="share-time">11:10AM</span>
-                            </div>
-                        </div>
-                    </div>
+            }
+            {
+                data &&
+                <div className='invoice-cnt-mobile' >
+
+                    <select className="invoice-top-bar-mobile" name="" id="">
+                        <option value="today">Today</option>
+                        <option value="last-week">Last week</option>
+                        <option value="last-month">Last Month</option>
+                        <option value="All">All</option>
+                    </select>
+                    {
+                        data.data.map(el => {
+                            return (
+                                <div className="invoice-card-mobile">
+                                    <div className="invoice-heading-mobile">
+                                        <span>Transactions Detail</span>
+                                    </div>
+                                    <hr />
+                                    <div className="invoice-item-detail-mobile">
+                                        <img className='' src={Store} alt='store'/>
+                                        <div>
+                                            <span id="invoice-business-mobile">{el.user.businessName}</span>
+                                            <span id="invoice-id-mobile"> id place-holder</span>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <div className="invoice-item-transaction-mobile">
+                                        <span className="i-trans-type-mobile">transcation-type</span>
+                                        <span id="i-trans-amount-mobile">$ 00.00</span>
+                                        <span className="i-trans-type-mobile">trans-mdeium</span>
+                                    </div>
+                                    <hr />
+                                    <div className="invoice-item-discription-mobile">
+                                        <div>description placeholder</div>
+                                    </div>
+                                    <hr />
+                                    <div className="invoice-bottom-mobile">
+                                        <button>Share</button>
+                                        <div>
+
+                                        <span>date-holder</span>
+                                        <span>time-holder</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                            })
+                        }
+
                 </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
